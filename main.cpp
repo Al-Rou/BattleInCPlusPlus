c0f25b97adfbf04d4d4fd19abb5fd740b6ec8402
Al-Rou <roal.24.alro@gmail.com>
2021-03-24 21:11:02 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -58,21 +58,72 @@ private:
     int life;
     bool loseStatus;
     int mana;
+    string choiceOfWeapon;
 public:
     Human(int a = 100, int b = 35);
     int getLife();
     bool getStatus();
     int getMana();
+    string getWeapon()
+    {
+        return choiceOfWeapon;
+    }
+    void setWeapon(string w)
+    {
+        choiceOfWeapon = w;
+    }
     int damage() override
     {
-
+        if (choiceOfWeapon == "Sword")
+        {
+            cout << "Weapon is Sword";
+        }
+        else if (choiceOfWeapon == "Fireball")
+        {
+            cout << "Weapon is Fireball";
+        }
     }
     void printData(int life, int mana, bool status);
 };
+Human::Human(int a, int b):
+    life(a),
+    mana(b),
+    loseStatus(false)
+    {}
+int Human::getLife()
+{
+    return life;
+}
+int Human::getMana()
+{
+    return mana;
+}
+bool Human::getStatus()
+{
+    return loseStatus;
+}
+void Human::printData(int life, int mana, bool status)
+{
+    if (status == false)
+    {
+        cout << "The life is " << life << " and the mana is " << mana << " and the lose status is false" << endl;
+    }
+    else
+    {
+        cout << "The life is " << life << " and the mana is " << mana << " and the lose status is true" << endl;
+    }
+}
 
 int main()
 {
     Monster* monCheck = new Monster();
+    Human* humanCheck = new Human();
     cout << monCheck->damage() << endl;
     monCheck->printData(monCheck->getLife(), monCheck->getStatus());
+    humanCheck->printData(humanCheck->getLife(), humanCheck->getMana(), humanCheck->getStatus());
+    cout << "What weapon?";
+    string answer;
+    cin >> answer;
+    humanCheck->setWeapon(answer);
+    humanCheck->damage();
 }
