9af2ca36faa5f39253235a71011da29e5a9d6abb
Al-Rou <roal.24.alro@gmail.com>
2021-03-24 20:34:50 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,16 +5,19 @@ using namespace std;
 
 class Player
 {
-private:
-    int life;
-    bool loseStatus;
 public:
-    int damage();
+    virtual int damage() = 0;
 };
 
 class Monster : public Player
 {
+private:
+    int life;
+    bool loseStatus;
 public:
+    Monster(int a = 100);
+    int getLife();
+    bool getStatus();
     int damage() override
     {
         int d = 0;
@@ -23,10 +26,35 @@ public:
         }while((d < 3) || (d > 14));
         return d;
     }
+    void printData(int life, bool loseStatus);
 };
+Monster::Monster(int a):
+    life(a),
+    loseStatus(false)
+    {}
+int Monster::getLife()
+{
+    return life;
+}
+bool Monster::getStatus()
+{
+    return loseStatus;
+}
+void Monster::printData(int life, bool loseStatus)
+{
+    if (loseStatus == false)
+    {
+        cout << "The life is " << life << " and the lose status is false" << endl;
+    }
+    else
+    {
+        cout << "The life is " << life << " and the lose status is true" << endl;
+    }
+}
 
 int main()
 {
     Monster* monCheck = new Monster();
-    cout << monCheck->damage();
+    cout << monCheck->damage() << endl;
+    monCheck->printData(monCheck->getLife(), monCheck->getStatus());
 }
